663d19eb69b8f31c1d0abd62f4acc1f4d0c8b7c0
YakovchukIvan <81960402+YakovchukIvan@users.noreply.github.com>
2024-03-12 22:15:17 +0200

    12345

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import styles from './Footer.module.scss';
 function Footer() {
   return (
     <div className={styles.footer}>
-      © 2020-2024 Kung Fu Sushi. Всі права захищено
+      <div></div>© 2020-2024 Kung Fu Sushi. Всі права захищено
     </div>
   );
 }
