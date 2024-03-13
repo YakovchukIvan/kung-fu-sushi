7cb46d2dd41d08d32c8725f6a8c0d8fd0f57985d
YakovchukIvan <81960402+YakovchukIvan@users.noreply.github.com>
2024-03-13 21:49:08 +0200

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,35 @@
+import { Link } from 'react-router-dom';
 import styles from './Footer.module.scss';
 
 function Footer() {
   return (
     <div className={styles.footer}>
-      <div></div>© 2020-2024 Kung Fu Sushi. Всі права захищено
+      <hr />
+      <div>
+        <div>
+          <Link to="/">
+            <h3>KUNG FU SUSHI</h3>
+          </Link>
+        </div>
+        <div>
+          <Link to="/">
+            <h4>Політика конфіденційності</h4>
+          </Link>
+        </div>
+        <div>
+          <Link to="/">
+            <h4>Доставка та ресторани</h4>
+          </Link>
+        </div>
+      </div>
+      <hr />
+      <div className={styles.protectText}>
+        <p>© 2020-2024 Kung Fu Sushi. Всі права захищено</p>
+        <div>
+          <img width={50} src="/img/visa-svgrepo.svg" alt="visa" />
+          <img width={50} src="/img/mastercard-svgrepo.svg" alt="mastercard" />
+        </div>
+      </div>
     </div>
   );
 }
