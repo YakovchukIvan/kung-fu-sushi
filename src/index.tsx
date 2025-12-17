d2aae29cd1fd33b57215afdcfc3234f038b31ec3
YakovchukIvan <81960402+YakovchukIvan@users.noreply.github.com>
2025-12-17 19:31:18 +0200

    Add react-app-env.d.ts for CRA TypeScript support

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,10 @@ import store from './redux/store';
 
 import './index.scss';
 
-const root = ReactDOM.createRoot(document.getElementById('root')!);
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
+
 root.render(
   <Provider store={store}>
     <Router>
